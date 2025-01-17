b924c66c5d52ed3d17e2c55710e58004f7abd03c
ht17d97 <63954538+ht17d97@users.noreply.github.com>
2025-01-17 21:41:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
-provider "azuread" {
-  # Ensure you are authenticated with sufficient permissions to create a user
-}
-
 resource "azuread_user" "example" {
-  user_principal_name = "john.doe@example.com" # Replace with the user's email
+  user_principal_name = "john.doe@himanshu.tariyal2025_outlook.com#EXT#@himanshutariyal2025outlook.onmicrosoft.com.
   display_name        = "John Doe"
   mail_nickname       = "johndoe"
-  password            = "P@ssw0rd123!" # Secure this in practice
+  password            = "P@ssw0rd123"
   force_password_change = true # Requires user to change password at first sign-in
 }
